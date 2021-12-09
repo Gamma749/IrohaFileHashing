5ec7bb129a614f41cdcfa2f1fa9568cdd013b3fa
Hayden McAlister <haydenmcalister49@gmail.com>
2021-12-09 15:16:35 +1300

    Update example scripts to fit new Custodian file

--- usr_scripts/NonupdatingFileHash.py
+++ usr_scripts/NonupdatingFileHash.py
@@ -1,6 +1,7 @@
 #! /bin/python
 
 from IrohaUtils import *
+from IrohaHashCustodian import Custodian
 from iroha import primitive_pb2
 import logging
 
@@ -8,58 +9,14 @@ import logging
 # INFO gives descriptions of steps
 # DEBUG gives information on transactions and queries too
 logging.basicConfig(level=logging.INFO)
-
-
-
-# The iroha domain that document hashes are stored in
-DEFAULT_DOMAIN_NAME = "document"
-
-# Create the users
-user_a = new_user("user_a", DEFAULT_DOMAIN_NAME)
-user_b = new_user("user_b", DEFAULT_DOMAIN_NAME)
-user_c = new_user("user_c", DEFAULT_DOMAIN_NAME)
-
-logging.info("Create new role \'document_creator\'\n\tand new domain \'document\'")
-commands = [
-    # Create a new role that can only create assets (i.e. create hashes) and read assets (to see if they exist)
-    iroha_admin.command("CreateRole", role_name="document_creator", permissions=[
-            primitive_pb2.can_create_asset,
-            primitive_pb2.can_read_assets
-        ]),
-    # Create a new role that can do NOTHING
-    iroha_admin.command("CreateRole", role_name="null_role", permissions=[
-        
-    ]),
-    # Create a new domain that has document_creator as role
-    iroha_admin.command("CreateDomain", domain_id=DEFAULT_DOMAIN_NAME, default_role="document_creator")
-]
-# Sign and send set up block
-tx = IrohaCrypto.sign_transaction(
-        iroha_admin.transaction(commands), ADMIN_PRIVATE_KEY)
-logging.debug(tx)
-status = send_transaction(tx, net_1)
-logging.debug(status)
-assert status[0] == "COMMITTED"
-logging.info("Domain created")
-
-logging.info("Create new users in domain")
-commands = [
-    iroha_admin.command('CreateAccount', account_name=user_a["name"], domain_id=DEFAULT_DOMAIN_NAME,
-                        public_key=user_a["public_key"]),
-    iroha_admin.command('CreateAccount', account_name=user_b["name"], domain_id=DEFAULT_DOMAIN_NAME,
-                        public_key=user_b["public_key"]),
-    iroha_admin.command('CreateAccount', account_name=user_c["name"], domain_id=DEFAULT_DOMAIN_NAME,
-                        public_key=user_c["public_key"])
-]
-tx = IrohaCrypto.sign_transaction(
-        iroha_admin.transaction(commands), ADMIN_PRIVATE_KEY)
-logging.debug(tx)
-status = send_transaction(tx, net_1)
-logging.debug(status)
-assert status[0] == "COMMITTED"
-logging.info("Users added")
-
-custodian = IrohaHashCustodian()
+logging.info("Create hash custodian")
+custodian = Custodian()
+logging.info("Create user A")
+user_a = custodian.new_hashing_user("user_a")
+logging.info("Create user B")
+user_b = custodian.new_hashing_user("user_b")
+logging.info("Create user C")
+user_c = custodian.new_hashing_user("user_c")
 
 
 logging.info("User A sends message1 to User B")
@@ -86,4 +43,4 @@ file_hash = custodian.get_file_hash("message3.secret")
 assert not custodian.find_hash_on_chain(user_c, file_hash)
 logging.info("User C did not find message3 hash on chain, verifies that message3 was not sent")
 
-log_all_blocks(net_1, f"node1.log")
+log_all_blocks("node1.log")
--- usr_scripts/UpdatingFileHash.py
+++ usr_scripts/UpdatingFileHash.py
@@ -1,60 +1,20 @@
 #! /bin/python
 
-from google.protobuf import message
 from IrohaUtils import *
-from iroha import primitive_pb2
+from IrohaHashCustodian import Custodian
 import logging
-import time
 
 
 # INFO gives descriptions of steps
 # DEBUG gives information on transactions and queries too
 logging.basicConfig(level=logging.INFO)
 
-
-
-# The iroha domain that document hashes are stored in
-DEFAULT_DOMAIN_NAME = "hashing"
-
-# Create the users
-user_a = new_user("user_a", DEFAULT_DOMAIN_NAME)
-user_b = new_user("user_b", DEFAULT_DOMAIN_NAME)
-
-logging.info(f"Create new roles \'document_creator\', \'null_role\'\n\tand new domain \'{DEFAULT_DOMAIN_NAME}\'")
-commands = [
-    # Create a new role that can only create assets (i.e. create hashes) and read assets (to see if they exist)
-    iroha_admin.command("CreateRole", role_name="document_creator", permissions=[
-            primitive_pb2.can_create_asset,
-            primitive_pb2.can_read_assets,
-        ]),
-    # Create a new domain that has document_creator as role
-    iroha_admin.command("CreateDomain", domain_id=DEFAULT_DOMAIN_NAME, default_role="document_creator")
-]
-# Sign and send set up block
-tx = IrohaCrypto.sign_transaction(
-        iroha_admin.transaction(commands), ADMIN_PRIVATE_KEY)
-logging.debug(tx)
-status = send_transaction(tx, net_1)
-logging.debug(status)
-assert status[0] == "COMMITTED"
-logging.info("Domain created")
-
-logging.info("Create new users in domain")
-commands = [
-    iroha_admin.command('CreateAccount', account_name=user_a["name"], domain_id=DEFAULT_DOMAIN_NAME,
-                        public_key=user_a["public_key"]),
-    iroha_admin.command('CreateAccount', account_name=user_b["name"], domain_id=DEFAULT_DOMAIN_NAME,
-                        public_key=user_b["public_key"])
-]
-tx = IrohaCrypto.sign_transaction(
-        iroha_admin.transaction(commands), ADMIN_PRIVATE_KEY)
-logging.debug(tx)
-status = send_transaction(tx, net_1)
-logging.debug(status)
-assert status[0] == "COMMITTED"
-logging.info("Users added")
-
-custodian = IrohaHashCustodian()
+logging.info("Create hash custodian")
+custodian = Custodian()
+logging.info("Create user A")
+user_a = custodian.new_hashing_user("user_a")
+logging.info("Create user B")
+user_b = custodian.new_hashing_user("user_b")
 
 logging.info("User A creates a message and logs it in a new domain")
 message = "Hello, World!"
@@ -78,4 +38,4 @@ logging.info("User B, having got the assets from the domain, checks the most rec
 assert message_hash==assets[-1]["hash"]
 logging.info("User B verifies that their message is the most recent on chain")
 
-log_all_blocks(net_1, f"node1.log")
+log_all_blocks("node1.log")
